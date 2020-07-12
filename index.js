e1f5f853235f2d7200eb7230b7f2e2366b0145eb
Fabricio Brito <fabricio.brito@gmail.com>
2020-07-12 20:42:30 -0300

    feat: Add Dispath Method to createState
    Dispatch method executes de reducer function and call subscribed funcs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function createStore(){
+//Library Code
+function createStore(reducer){
   let state; //The state itself
   let listeners = []; //The list of listeners subscribed to be notified when state changes
 
@@ -12,9 +13,44 @@ function createStore(){
       listeners = listeners.filter((l) => l.id !== listener.id)
     })
   }
+
+  //3. Update de state and call all subscribed functions
+  const dispatch = (action) => {
+    state = reducer(action);
+    listeners.forEach((listener) => listener());
+  }
+
   // createStore will return the function handlers to deal with the app state
   return(
     getState,
-    subscribe
+    subscribe,
+    dispatch
   )
 }
+
+//App Code
+//Reducer ToDo's (Pure Function)
+function todos (state = [], action){
+  switch (action.type){
+    case 'ADD_TODO':
+      return state.concat([action.todo]);
+    default:
+      return state;
+  }
+}
+
+//Usage Example
+const store = createStore(todos);
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+
+store.dispatch(store.getState(), {
+  type: 'ADD_TODO',
+  todo:{
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
+})
